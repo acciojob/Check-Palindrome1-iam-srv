527abb4ee9406b941af7049914d19fe0d4642037
SRIRAM VISWANATHAN <117807050+iam-srv@users.noreply.github.com>
2023-05-14 22:15:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,8 @@
 function palindrome(str){
    let start = 0;
    let end  = str.length;
-
+   str.replace(" " , "");
+   str.toLowerCase();
 	while(start < end){
 		if(str.charAt(start) === str.charAt(end)){
 			start++;
