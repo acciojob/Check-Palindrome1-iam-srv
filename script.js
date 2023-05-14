fc24b97f651f0013216b2467daaf2133aca75467
SRIRAM VISWANATHAN <117807050+iam-srv@users.noreply.github.com>
2023-05-14 22:14:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 // complete the given function
 
 function palindrome(str){
+   let start = 0;
+   let end  = str.length;
 
+	while(start < end){
+		if(str.charAt(start) === str.charAt(end)){
+			start++;
+			end--;
+		}else {
+			return false;
+		}
+	}
+
+	return true;
 }
 module.exports = palindrome
